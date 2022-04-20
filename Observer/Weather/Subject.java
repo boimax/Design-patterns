8c1add6417169423fb01e8f9e6c25ccf8dacc43e
boimax <hocattuong2404@gmail.com>
2022-04-20 14:22:25 +0700

    Add temperature files

---
+++ Observer/Weather/Subject.java
@@ -0,0 +1,9 @@
+package Observer.Weather;
+
+public interface Subject {
+    public void addObserver(Observer observer);
+
+    public void removeObserver(Observer observer);
+
+    public void notifyObservers();
+}
