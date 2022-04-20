35553722900a21052aa38deb9d81501c1288f756
boimax <hocattuong2404@gmail.com>
2022-04-20 15:06:32 +0700

    Add 4th Observer_Weather

--- Observer/Weather/WeatherStation.java
+++ Observer/Weather/WeatherStation.java
@@ -4,6 +4,8 @@ public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statistctDisplay = new StatisticsDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
