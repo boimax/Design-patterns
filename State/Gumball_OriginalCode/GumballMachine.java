d7bafb80830778e76200004c82ff5cedf3643b40
boimax <hocattuong2404@gmail.com>
2022-04-21 09:18:17 +0700

    Updated

--- State/Gumball_OriginalCode/GumballMachine.java
+++ State/Gumball_OriginalCode/GumballMachine.java
@@ -74,4 +74,31 @@ public class GumballMachine {
             System.out.println("Turning twice doesn't get you another gumball!");
         }
     }
+
+    public void refill(int newNumBalls) {
+        state = NO_QUARTER;
+        this.count = newNumBalls;
+    }
+
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("\nMighty Gumball, Inc.");
+        result.append("\nJava-enabled Standing Gumball Model #2004\n");
+        result.append("Inventory: " + count + " gumball");
+        if (count != 1) {
+            result.append("s");
+        }
+        result.append("\nMachine is ");
+        if (state == SOLD_OUT) {
+            result.append("sold out");
+        } else if (state == NO_QUARTER) {
+            result.append("waiting for quarter");
+        } else if (state == HAS_QUARTER) {
+            result.append("waiting for turn of crank");
+        } else if (state == SOLD) {
+            result.append("delivering a gumball");
+        }
+        result.append("\n");
+        return result.toString();
+    }
 }
