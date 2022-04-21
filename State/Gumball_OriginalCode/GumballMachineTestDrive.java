5f4c24fef285aba079f007b967799870bd5ba38d
boimax <hocattuong2404@gmail.com>
2022-04-21 09:19:30 +0700

    Client file

---
+++ State/Gumball_OriginalCode/GumballMachineTestDrive.java
@@ -0,0 +1,12 @@
+package State.Gumball_OriginalCode;
+
+public class GumballMachineTestDrive {
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+    }
+}
