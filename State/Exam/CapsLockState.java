b07c3e44c868e72379210b85aeec94050caf1adc
boimax <hocattuong2404@gmail.com>
2022-04-26 11:26:49 +0700

    Finish State interface and 2 concrete states

--- State/Exam/CapsLockState.java
+++ State/Exam/CapsLockState.java
@@ -4,7 +4,7 @@ public class CapsLockState implements State {
         switch (key) {
             case CAPSLOCK:
                 keyboard.setState(new NormalState());
-                System.out.println("CapsLockState OFF");
+                System.out.println("CapsLockState OFF ");
                 break;
             default:
                 System.out.println(key.getvalue().toUpperCase());
