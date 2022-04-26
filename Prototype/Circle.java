ac29bb9eeef65a5274690e7eae1175d5ebcc1a6a
boimax <hocattuong2404@gmail.com>
2022-04-26 18:14:29 +0700

    Finish abstract class Shape and 3 concrete shape classes

---
+++ Prototype/Circle.java
@@ -0,0 +1,12 @@
+public class Circle extends Shape {
+
+    public Circle() {
+        type = "Circle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+
+}
