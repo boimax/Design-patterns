00188ebe317600e1c171a575ac1d7416ff56ef01
boimax <hocattuong2404@gmail.com>
2022-04-26 18:31:11 +0700

    Finish khuong ShapeCache

--- Prototype/ShapeCache.java
+++ Prototype/ShapeCache.java
@@ -3,13 +3,13 @@ import java.util.HashMap;
 public class ShapeCache {
     private static HashMap<String, Shape> shapeMap = new HashMap<String, Shape>();
 
+    public static Shape getShape(String shapeID) {
+        return (Shape) shapeMap.get(shapeID).clone();
+    }
+
     public static void loadCache() {
         shapeMap.put("1", new Circle());
         shapeMap.put("2", new Rectangle());
         shapeMap.put("3", new Square());
-    }
-
-    public static Shape getShape(String shapeID) {
-        return (Shape) shapeMap.get(shapeID).clone();
     }
 }
