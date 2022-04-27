1d4ff666a054cc774395dd092f66782b0f1deb2b
boimax <hocattuong2404@gmail.com>
2022-04-27 14:51:16 +0700

    Lazy initialization

---
+++ Singleton/LazySingleton.java
@@ -0,0 +1,21 @@
+/*
+Object is created only if it is needed. It may overcome resource overcome and wastage of CPU time.
+Exception handling is also possible in method.
+Every time a condition of null has to be checked.
+instance can’t be accessed directly.
+In multithreaded environment, it may break singleton property. -CONS
+*/
+public class LazySingleton {
+    private LazySingleton() {
+        // private constructor
+    }
+
+    private static LazySingleton instance; // private static variable
+
+    private static LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
