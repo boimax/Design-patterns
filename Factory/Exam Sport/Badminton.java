e99194d4310a53b47fb6eb5668dcf8c323da21f8
boimax <hocattuong2404@gmail.com>
2022-04-27 15:17:54 +0700

    Finish Sport interface and 2 concrete sport classes

--- Factory/Exam Sport/Badminton.java
+++ Factory/Exam Sport/Badminton.java
@@ -1,3 +1,4 @@
+//
 public class Badminton implements Sport {
     public Badminton() {
         System.out.println("A badminton object has been created");
--- Factory/Exam Sport/Zumba.java
+++ Factory/Exam Sport/Zumba.java
@@ -1,3 +1,4 @@
+//
 public class Zumba implements Sport {
     public Zumba() {
         System.out.println("A zumba object has been created");
